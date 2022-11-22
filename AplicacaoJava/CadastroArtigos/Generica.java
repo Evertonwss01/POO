7e7873a6e793e9c53674a8a1cc96486f28853866
Evertonwss01 <evertonwss2001@gmail.com>
2022-11-22 17:26:36 -0300

    Criacao_Modelos
    Criação das Classes generica Usuario e Artigo

---
+++ AplicacaoJava/CadastroArtigos/Generica.java
@@ -0,0 +1,22 @@
+class Generica{
+    
+    // Atributos //
+    private int id;
+    private String uuid;
+    private String dataRegistro;
+
+    // Metodos Especiais //
+    public int getId(){
+        return id;
+    }public void setId(int id){
+        this.id = id;
+    }
+    public String getUuid(){
+        return uuid;
+    }public void setUuid(String uuid){
+        this.uuid = uuid;
+    }
+    public String getDataRegistro(){
+        return dataRegistro;
+    }
+}
